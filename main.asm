27d3cf7535f587b4f07451db313873500d16c994
BIG-KG <bek.georgy@gmail.com>
2025-02-17 19:21:08 +0300

    delete garbage

--- main.asm
+++ main.asm
@@ -8,22 +8,68 @@ org 100h
 
 .code
 
-main: proc
+main proc
 
-    mov ax, 00
+
+    mov ax, 00              ; calculate 09h interraption location in memory
     mov es, ax
     mov bx, 09h * 4
-    mov es:[bx], offset New9hInterrupt
+    
+    mov ax, es:[bx]         ; previous 09h value -> [Delta]
+    mov Delta, ax
 
+    cli
+    mov es:[bx], offset New9hInterrupt ; New9hInterrupt -> 09h 
+    mov ax, cs
+    mov es:[bx], ax
+    sti
 
-New9hInterrupt: proc
-
-    mov ax, 
-
+    mov ax, 3100h
+    mov dx, offset endProg
+    shr dx, 4
+    inc dx
+    int 21h
 
     endp
 
-                
 
+New9hInterrupt proc
+    push ax         ;   save ax, es
+    push es  
+    push bx 
+
+    mov ax, 0b800h      ;   videoseg -> es
+    mov es, ax
+
+    mov bx, 4 * 80 * 2  ;   target drawing Position -> bx
+    add bx, 4 * 2
+
+    in al, 60h          ; 60h port -> al -> es:[bx] videoSeg bx coord
+    mov es:[bx], al
+
+    in al, 61h 
+    mov ah, al
+    or al, 80h
+    out 61h, al
+    xchg 21h
+    mov al, ah
+    out 61h, al
+    mov al, 20h
+    out 20h, al
+
+    pop bx
+    pop es
+    pop ax
+    iret
+    endp
+    
+
+OurJmp  db 0eah
+        dw 0
+Delta   dw 0
+
+
+
+endProg:
 
 end main
